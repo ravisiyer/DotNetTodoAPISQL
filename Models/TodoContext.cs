243dbea9761226da784030d7d0c765365ed812f3
ravisiyer <145967375+ravisiyer@users.noreply.github.com>
2024-11-16 23:49:02 +0530

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace TodoAPISQL.Models;
+
+public class TodoContext : DbContext
+{
+    //public TodoContext()
+    //    : base()
+    //{
+    //}
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    // //   warning To protect potentially sensitive information in your connection string,
+    // //           you should move it out of source code.See http://go.microsoft.com/fwlink/?LinkId=723263 
+    // //for guidance on storing connection strings.
+
+    //    optionsBuilder.UseMySQL("server=localhost;database=test;user=root;password=");
+    //}
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoAPISQL.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
